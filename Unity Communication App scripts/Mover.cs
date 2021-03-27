40c94a4a2c47de7bd40717093ce953afb81c2e88
shwinn08 <81485314+shwinn08@users.noreply.github.com>
2021-03-27 23:52:14 +0530

    Add files via upload

---
+++ Unity Communication App scripts/Mover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]float moveSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintInstructions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePLayer();
+    }
+     
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to Room 1");
+        
+
+    }
+    void MovePLayer()
+        {
+            float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+            float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+            transform.Translate(xValue,0,zValue);  
+        }
+    
+}
